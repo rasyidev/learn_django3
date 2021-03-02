6fa791e500cc04ee86bcfe38c6275daeae2ae8fb
rasyidev <habib.rasyid11@gmail.com>
2021-03-02 13:02:23 +0700

    Create simple user authentication to access certain pages

--- Django - Kelas Terbuka/54-Authentikasi_User_Login_Logout/Django_Project/mywebsite/urls.py
+++ Django - Kelas Terbuka/54-Authentikasi_User_Login_Logout/Django_Project/mywebsite/urls.py
@@ -5,7 +5,9 @@ from .import views
 
 # app_name = "mywebsite"
 urlpatterns = [
-    path('login/', views.index, name='login'),
+    path('', views.index, name='home'),
+    path('login/', views.user_login, name='login'),
+    path('user-area/', views.user_area, name='user_area'),
     path('logout/', views.logout, name='logout'),
     path('admin/', admin.site.urls),
 ]
--- Django - Kelas Terbuka/54-Authentikasi_User_Login_Logout/Django_Project/mywebsite/views.py
+++ Django - Kelas Terbuka/54-Authentikasi_User_Login_Logout/Django_Project/mywebsite/views.py
@@ -1,33 +1,54 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout as dj_logout
+from django.contrib.auth.decorators import login_required
+
 
 
 def index(request):
-   
-   if(request.method == 'POST'):
-      username = request.POST['username']
-      password = request.POST['password']
-
-      user = authenticate(request, username=username, password=password)
-      print(user)
-      if user is not None:
-         login(request, user)
-         print(user)
-
    context = {
-      'page_title': "Login Page"
+      'page_title': "Home Page"
    }
    return render(request, 'index.html', context)
 
+def user_login(request):
+   if(request.method == 'POST'):
+      username = request.POST['username']
+      password = request.POST['password']
+      user = authenticate(request, username=username, password=password)
+
+      print(user)
+      if user is not None:
+         if(user.is_active):
+            login(request, user)
+            return redirect('user_area')
+         else:
+            return redirect('home')
+
+   context ={
+      'page_title': "Login Page"
+   }
+
+   if(request.method == 'GET'):
+      if(request.user.is_authenticated):
+         return redirect('user_area')
+      else:
+         return render(request, 'login.html', context)
+
+@login_required
 def logout(request):
-   dj_logout(request)
-   return redirect('index')
-   # context = {
-   #    'page_title': "Halaman Logout"
-   # }
-   # if request.method == 'POST':
-      # print(request.POST['logout'] == 'Submit')
-   #    if request.POST['logout'] == 'Submit':
-   #       logout(request)
-   #    # return redirect('login')
-   # return render(request, 'logout.html', context)
+   context = {
+      'page_title': "Halaman Logout"
+   }
+   if request.method == 'POST':
+      print(request.POST['logout'] == 'Submit')
+      if request.POST['logout'] == 'Submit':
+         dj_logout(request)
+         return redirect('home')
+   return render(request, 'logout.html', context)
+
+@login_required
+def user_area(request):
+   context = {
+      'page_title': "User Area"
+   }
+   return render(request, 'user_area.html', context)
